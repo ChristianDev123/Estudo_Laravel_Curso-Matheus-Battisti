4cafccf7b0fa2fa1cd67c1147378fb180c88c426
ChristianDev123 <christian.lima999@gmail.com>
2022-05-31 22:45:23 -0300

    aula 13 e 14

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,28 @@
 @extends('layouts.main')
 @section('titlePage','Laravel Page')
 @section('contentPage')
-    <h1>Algum título</h1>
-    <a href="/contacts">
-        <img src="/img/banner.jpg" alt="">
-    </a>
-    <script src="/js/scripts.js"></script>
+    
+<div id="search-container" class="col-md-12">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+    </form>
+</div>
+<div id="events-container" class="col-md-12">
+    <h2>Próximos Eventos</h2>
+    <p class="subtitle">Veja os eventos dos próximos dias</p>
+    <div id="cards-container" class="row">
+        @foreach($events as $event)
+            <div class="card col-md-3">
+                <img src="/img/event_placeholder.jpg" alt="{{ $event->title }}">
+                <div class="card-body">
+                    <p class="card-date">10/09/2020</p>
+                    <h5 class="card-title">{{$event->title}}</h5>
+                    <p class="card-participants">x Participantes</p>
+                    <a href="#" class="btn btn-primary">Saber mais</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,6 @@ Route::get('/',[EventController::class,'index']);
 
 Route::get('/events/create',[EventController::class,'create']);
 
-Route::get('/contacts',fn()=>view('contact'));
-
-Route::get('/produtos/{id}',function($id=''){
-    $busca = request('search');
-    return view('product',[
-        'id'=>$id,
-        'busca'=>$busca
-    ]);
+Route::get('/contact',function(){
+    return view('contact');
 });
